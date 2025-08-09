1d89eedbea3925b8320728b63ea2f20994b304a7
Rajan Kalyan <swami.rajan.kalyan@gmail.com>
2025-08-09 18:01:07 +0545

    bugs fix

--- config/database.php
+++ config/database.php
@@ -21,6 +21,21 @@ return [
             'sslmode' => env('DB_SSLMODE', 'prefer'),
         ],
 
+        'cas_user' => [
+            'driver' => 'pgsql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'one_system'),
+            'username' => env('DB_USERNAME', 'cas_user'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'search_path' => 'public',
+            'sslmode' => env('DB_SSLMODE', 'prefer'),
+        ],
+
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
